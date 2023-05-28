233e3489d5e6227ea9334a369b082a84f45c4e62
jhonshua <julio.llinas@gmail.com>
2023-05-28 18:14:50 -0400

    fix db mongose

--- index.js
+++ index.js
@@ -24,7 +24,11 @@ const app = express();
 app.use('/uploads', express.static(__dirname + '/uploads'));
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors());
+
+app.use(cors({
+  credentials: true,
+  origin: " https://react-chat-gilt.vercel.app/",
+}));
 
 async function getUserDataFromRequest(req) {
   return new Promise((resolve, reject) => {
